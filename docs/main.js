d2e85b7587ac8ec035a12cd22fae6d3356dbee8d
Bryan "Beege" Berry <bryanberry235711@gmail.com>
2025-05-31 20:27:12 +0900

    feat: Read-write to local storage works for fields

---
+++ docs/main.js
@@ -0,0 +1,42 @@
+/**
+ * This is linked using the html `defer` attribute, so `onload` nonsense isn't necessary.
+ */
+
+/**
+ * This reads data from the triggering form event and persists it to browser storage.
+ * 
+ * @param {Event} event
+ */
+function persistFormInput(event) {
+    const fieldName = event.target.getAttribute("name");
+    const fieldValue = event.target.value;
+    localStorage.setItem(fieldName, fieldValue);
+}
+
+/**
+ * This checks local storage for an existing value and populates that field if the 
+ * corresponding value is found.
+ * 
+ * @param {(HTMLInputElement|HTMLSelectElement)} field 
+ */
+function fillFieldFromStorage(field) {
+    const fieldName = field.getAttribute("name");
+    const fieldValue = localStorage.getItem(fieldName);
+    field.value = fieldValue;
+}
+
+// Fetch every field
+const allFields = document.querySelectorAll("input:not(:disabled):not([readonly]), " + 
+    "select:not(:disabled):not([readonly])");
+
+for (const field of allFields) {
+    // Load existing data from local storage
+    fillFieldFromStorage(field);
+
+    // Trigger persisting data for every field
+    if (field.tagName === "select") {
+        field.addEventListener("change", persistFormInput);
+    } else if (field.tagName === "input") {
+        field.addEventListener("focusout", persistFormInput);
+    }
+}
